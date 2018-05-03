2d31092495522cea5e804273226b5598ce0039fb
Victor Rezende dos Santos <victor.rezende@outlook.com>
2018-05-03 07:50:05 -0300

    Fix Django Secret EnvVar

--- debian/slim-stretch/entrypoint.sh
+++ debian/slim-stretch/entrypoint.sh
@@ -14,10 +14,6 @@ if [ ! -z "$@" ]; then
     exec "$@"
 fi
 
-#if [ ! -e "${ETESYNC_SECRET}" ]; then
-#    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > ${ETESYNC_SECRET}
-#fi
-
 if [ ! -e "${ETESYNC_DB_PATH}" ]; then
     # first run
     $ETESYNC_PATH/manage.py migrate
